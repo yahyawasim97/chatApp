2b82dffc38a5804805af31b30a86b9da0bbf0810
Yahya Wasim <yahya.wasim@thevisionspark.com>
2018-12-11 13:52:33 +0500

    Basic Setup

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const http = require('http');
+const container = require('./container');
+
+container.resolve(function(users){
+    const app = SetupExpress();
+    function SetupExpress(){
+        const app = express();
+        const server = http.createServer(app);
+        server.listen(3000,function(){
+            console.log('Listening on port 3000');
+        })
+    }
+    //Setup router
+    const router = require('express-promise-router')();
+    users.SetRouting(router); 
+    app.use(router);
+});
